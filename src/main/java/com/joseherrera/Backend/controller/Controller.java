b8ba454f219c5bbd721e8daa8b508d22b7f89baa
Jose Herrera <jooherrera4@gmail.com>
2022-05-17 13:44:04 -0300

    Home API Controller

--- src/main/java/com/joseherrera/Backend/controller/Controller.java
+++ src/main/java/com/joseherrera/Backend/controller/Controller.java
@@ -1,15 +1,18 @@
 package com.joseherrera.Backend.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1")
 public class Controller {
-    
+
+    @Value("${environment.name}")
+    private String envName;
+
     @GetMapping("/")
-    public String home(){
-        return "Home API - V1";
+    public String home() {
+
+        return "Home API - V1 -- " + envName;
     }
 }
